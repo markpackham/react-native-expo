d246f40ccd702e648ace8620ebdad22b2e104e68
M <markpackham1@gmail.com>
2025-02-28 16:57:03 +0000

    Change layout section to use arrow function
    https://youtu.be/bCpFbERgj7s?feature=shared&t=1310

--- app/index.jsx
+++ app/index.jsx
@@ -1,19 +1,19 @@
 import { Text, View, StyleSheet } from "react-native";
 
-export default function Index() {
+const HomeScreen = () => {
   return (
-    <View
-      style={styles.container}
-    >
+    <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default HomeScreen;
