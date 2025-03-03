782cc14a6bca984ce86de04bbab2a7bd90af8ac8
M <markpackham1@gmail.com>
2025-03-03 10:52:57 +0000

    Add styling for NoteItem
    https://youtu.be/bCpFbERgj7s?feature=shared&t=3176

--- components/NoteItem.jsx
+++ components/NoteItem.jsx
@@ -1,11 +1,37 @@
-import { View, Text } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 
-const NoteItem = ({note}) =>{
-    return (
-        <View style={styles.noteItem}>
-        <Text style={styles.noteText}>{note.text}</Text>
-      </View>
-    );
-}
+const NoteItem = ({ note }) => {
+  return (
+    <View style={styles.noteItem}>
+      <Text style={styles.noteText}>{note.text}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  noteItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#f5f5f5",
+    padding: 15,
+    borderRadius: 5,
+    marginVertical: 5,
+  },
+  noteText: {
+    fontSize: 18,
+  },
+  delete: {
+    fontSize: 18,
+    color: "red",
+  },
+  actions: {
+    flexDirection: "row",
+  },
+  edit: {
+    fontSize: 18,
+    marginRight: 10,
+    color: "blue",
+  },
+});
 
 export default NoteItem;
