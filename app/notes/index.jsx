c78dd1615678e4b3731f9d1f79f22cff402e6d20
M <markpackham1@gmail.com>
2025-03-03 09:41:06 +0000

    Add notes state https://youtu.be/bCpFbERgj7s?feature=shared&t=2139

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 const NoteScreen = () => {
+  const [notes, setNotes] = useState([
+    {id: '1', text:'Note one'},
+    {id: '2', text:'Note two'},
+    {id: '3', text:'Note two'},
+    {id: '4', text:'Note two'},
+  ])
+
   return (
     <View style={styles.container}>
       <Text>Notes</Text>
