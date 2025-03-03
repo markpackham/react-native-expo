284233e61ca78eb7502aa330577beb63466bc759
M <markpackham1@gmail.com>
2025-03-03 09:51:19 +0000

    Style the notes https://youtu.be/bCpFbERgj7s?feature=shared&t=2287

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, FlatList, Text, StyleSheet } from "react-native";
 
 const NoteScreen = () => {
   const [notes, setNotes] = useState([
-    {id: '1', text:'Note one'},
-    {id: '2', text:'Note two'},
-    {id: '3', text:'Note two'},
-    {id: '4', text:'Note two'},
-  ])
+    { id: "1", text: "Note one" },
+    { id: "2", text: "Note two" },
+    { id: "3", text: "Note three" },
+    { id: "4", text: "Note four" },
+  ]);
 
   return (
     <View style={styles.container}>
-      <Text>Notes</Text>
+      <FlatList
+        data={notes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.noteItem}>
+            <Text style={styles.noteText}>{item.text}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
@@ -21,6 +29,17 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     backgroundColor: "#fff",
+  },
+  noteItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#f5f5f5",
+    padding: 15,
+    borderRadius: 5,
+    marginVertical: 5,
+  },
+  noteText: {
+    fontSize: 18,
   },
 });
 
