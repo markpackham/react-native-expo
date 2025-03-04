21c301c22230703b941c32173c7793d0c59d7055
M <markpackham1@gmail.com>
2025-03-04 10:31:25 +0000

    Layout tweak appwrite

--- services/appwrite.js
+++ services/appwrite.js
@@ -2,7 +2,7 @@ import { Client, Databases } from "react-native-appwrite";
 import { Platform } from "react-native";
 
 const config = {
-  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENPOINT,
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
   db: process.env.EXPO_PUBLIC_APPWRITE_DB_ID,
   col: {
