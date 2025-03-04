4570a84cce4989b920c6d5ecf941db3badead751
M <markpackham1@gmail.com>
2025-03-04 12:15:09 +0000

    Add addNote function
    https://youtu.be/bCpFbERgj7s?feature=shared&t=5134

--- services/noteService.js
+++ services/noteService.js
@@ -1,5 +1,5 @@
-import databaseService from './databaseService';
-import { ID, Query } from 'react-native-appwrite';
+import databaseService from "./databaseService";
+import { ID, Query } from "react-native-appwrite";
 
 // Appwrite database & collection id
 const dbId = process.env.EXPO_PUBLIC_APPWRITE_DB_ID;
@@ -13,7 +13,34 @@ const noteService = {
       return { error: response.error };
     }
 
-    return {data: response}
+    return { data: response };
+  },
+
+  // Add new note
+  async addNote(text) {
+    if (!text) {
+      return { error: "Note text cannot be empty" };
+    }
+
+    const data = {
+      text: text,
+      createdAt: new Date().toISOString(),
+    };
+    const response = await databaseService.createDocument(
+      dbId,
+      colId,
+      data,
+      ID.unique()
+    );
+
+    /*
+optional chaining (?.) operator accesses an object's property or calls a function. If the object accessed or function called using this operator is undefined or null
+    */
+    if (response?.error) {
+      return { error: response.error };
+    }
+
+    return {data:response}
   },
 };
 
