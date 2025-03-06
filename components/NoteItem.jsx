69cab4f2785329286507efaacbbda824d8121d97
M <markpackham1@gmail.com>
2025-03-06 11:08:25 +0000

    Add edit functionality in NoteItem
    https://youtu.be/bCpFbERgj7s?feature=shared&t=6658

--- components/NoteItem.jsx
+++ components/NoteItem.jsx
@@ -1,12 +1,46 @@
-import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { useRef, useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 
 const NoteItem = ({ note, onDelete }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedText, setEditedText] = useState(note.text);
+  const inputRef = useRef(null);
+
   return (
     <View style={styles.noteItem}>
-      <Text style={styles.noteText}>{note.text}</Text>
-      <TouchableOpacity onPress={() => onDelete(note.$id)}>
+      {isEditing ? (
+        <TextInput
+          ref={input}
+          style={styles.input}
+          value={editedText}
+          onChangeText={setEditedText}
+          autoFocus
+          onSubmitEditing={handleSave}
+          returnKeyType="done"
+        />
+      ) : (
+        <Text style={styles.noteText}>{note.text}</Text>
+      )}
+      <View style={styles.actions}>
+        {isEditing ? (
+          <TouchableOpacity onPress={()=> {handleSave(); inputRef.current?.blur()}}>
+            <Text styles={styles.edit}>SAVE</Text>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity onPress={() => setIsEditing(true)}>
+          <Text style={styles.edit}>EDIT</Text>
+        </TouchableOpacity>
+        )}
+        <TouchableOpacity onPress={() => onDelete(note.$id)}>
           <Text style={styles.delete}>DELETE</Text>
         </TouchableOpacity>
+      </View>
     </View>
   );
 };
