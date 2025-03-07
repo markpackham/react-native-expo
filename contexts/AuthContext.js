2fb0a25f104e1a5994b5ce6d0cb6326df9cc91b7
M <markpackham1@gmail.com>
2025-03-07 11:21:24 +0000

    Add AuthContext with an authentication provider
    https://youtu.be/bCpFbERgj7s?si=DWreH8JZYLiuIEJ5&t=7740

---
+++ contexts/AuthContext.js
@@ -0,0 +1,27 @@
+import { createContext, useContext, useState, useEffects } from "react";
+import authService from "@/services/authService";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
+  const checkUser = async () => {
+    setLoading(true);
+    const response = await authService.getUser();
+
+    if (response?.error) {
+      // If an error, the user not logged in set them to null
+      setUser(null);
+    } else {
+      setUser(response);
+    }
+
+    setLoading(false);
+  };
+};
