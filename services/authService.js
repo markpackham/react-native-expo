8a30f4203c27231b83bb1cdbcc5526ebdb6170fb
M <markpackham1@gmail.com>
2025-03-10 11:48:41 +0000

    Correct authService which doesn't have messages

--- services/authService.js
+++ services/authService.js
@@ -1,58 +1,51 @@
-import { ID } from "react-native-appwrite";
 import { account } from "./appwrite";
+import { ID } from "react-native-appwrite";
 
 const authService = {
-
   // Register a user
   async register(email, password) {
     try {
-      const response = await account.create(ID.unique, email, password);
+      const response = await account.create(ID.unique(), email, password);
       return response;
     } catch (error) {
-      console.error("Error authenticating", message);
       return {
-        error: error.message || "Registeration failed, please try again.",
+        error: error.message || "Registration failed. Please try agian",
       };
     }
   },
-
   // Login
   async login(email, password) {
     try {
-      const response = await account.createEmailPasswordSession(email, password);
+      const response = await account.createEmailPasswordSession(
+        email,
+        password
+      );
       return response;
     } catch (error) {
-      console.error("Error logging in", message);
       return {
-        error: error.message || "Login failed, please try again.",
+        error: error.message || "Login failed. Please check your credentials",
       };
     }
   },
-
   // Get logged in user
-  async getUser(email, password) {
+  async getUser() {
     try {
       return await account.get();
     } catch (error) {
-      console.error("Error getting user");
-      return {
-        error: error.message || "Failed to get user, please try again.",
-      };
+      return null;
     }
   },
 
-  // Logout
-  async logout(){
+  // Logout user
+  async logout() {
     try {
-        // Removes current user session
-        await account.deleteSession('current')
+      await account.deleteSession("current");
     } catch (error) {
-        console.error("Error logging user out");
-        return {
-          error: error.message || "Failed to logout user, please try again.",
-        };
+      return {
+        error: error.message || "Logout failed. Please try again",
+      };
     }
-  }
+  },
 };
 
 export default authService;
