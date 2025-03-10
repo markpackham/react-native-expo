6410e496118dec5cc92dcd1365cc20c21a5298ab
M <markpackham1@gmail.com>
2025-03-10 13:44:21 +0000

    Pass query for user id into databaseService
    https://youtu.be/bCpFbERgj7s?si=daMFgSbHOPXgYKQr&t=10443

--- services/databaseService.js
+++ services/databaseService.js
@@ -4,12 +4,12 @@ import { database } from "./appwrite";
 
 const databaseService = {
   // List documents taking in databse id and colleciton id (a NoSQL version of a table)
-  async listDocuments(dbId, colId) {
+  async listDocuments(dbId, colId, queries = []) {
     try {
       // .listDocuments part of SDK of database object
       // this differs from the async function above which we could call getDocuments if we liked
-      const response = await database.listDocuments(dbId, colId);
-      return response.documents || [];
+      const response = await database.listDocuments(dbId, colId, queries);
+      return {data: response.documents || [], error: null};
     } catch (error) {
       console.error("Error fetching documents", error.message);
       return { error: error.message };
