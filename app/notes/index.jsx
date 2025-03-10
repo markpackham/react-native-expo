16aa15bf9be49251844fbfd3a4840986ba6bed00
M <markpackham1@gmail.com>
2025-03-10 13:58:59 +0000

    Tell user if they have no notes
    https://youtu.be/bCpFbERgj7s?si=pEY7zmOohwK7uOvo&t=10635

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -110,7 +110,12 @@ const NoteScreen = () => {
       ) : (
         <>
           {error && <Text style={styles.errorText}>{error}</Text>}
-          <NoteList notes={notes} onDelete={deleteNote} onEdit={editNote} />
+
+          {notes.length === 0 ? (
+            <Text style={styles.noNotesText}>You do not have any notes</Text>
+          ) : (
+            <NoteList notes={notes} onDelete={deleteNote} onEdit={editNote} />
+          )}
         </>
       )}
 
